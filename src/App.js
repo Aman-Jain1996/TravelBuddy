8bf04b81e1a131e581654115618336a245fbe645
Aman Jain <67225022+Aman-Jain1996@users.noreply.github.com>
2021-07-04 18:33:59 +0530

    feature : header and country list Added country list and header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,59 @@
 import "./styles.css";
+import { useState } from "react";
+import travelDB from "./db.js";
+
+const country = Object.keys(travelDB);
 
 export default function App() {
-  return <div></div>;
+  const [selectedCountry, setSelectedCountry] = useState("India");
+  function ClickHandler(item) {
+    setSelectedCountry(item);
+  }
+
+  return (
+    <div>
+      <header>
+        <div>TravelBuddy</div>
+      </header>
+
+      <div className="hero hero-container">
+        <div className="desc-container">
+          Love Travelling.....
+          <br /> Check our recommendation
+        </div>
+        <div className="image-container">
+          <img src="images/hero.svg" alt="Logo" />
+        </div>
+      </div>
+
+      <div className="country-container">
+        {country.map((item) => {
+          return (
+            <div
+              className="btn-country"
+              key={item}
+              onClick={() => ClickHandler(item)}
+            >
+              {item}
+            </div>
+          );
+        })}
+      </div>
+
+      <hr />
+
+      <div>
+        {travelDB[selectedCountry].map((item) => {
+          return (
+            <div className="hero-container" key={item.city}>
+              <div className="image-container">
+                <img alt="City" src={item.poster} />
+              </div>
+              <div className="desc-container"></div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
